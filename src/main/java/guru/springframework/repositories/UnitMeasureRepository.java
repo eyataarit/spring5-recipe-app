b2273c6a0b2722c45136c11b33e7608ae4ac39f6
Aya Ben Taarit <taarit.aya@gmail.com>
2022-06-02 16:57:16 +0200

    create repositories

---
+++ src/main/java/guru/springframework/repositories/UnitMeasureRepository.java
@@ -0,0 +1,8 @@
+package guru.springframework.repositories;
+
+import guru.springframework.model.UnitMeasure;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UnitMeasureRepository extends CrudRepository<UnitMeasure, Long> {
+
+}

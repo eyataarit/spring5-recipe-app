8095d77f9e97fb09224b4c79f19cea4cf8b36c26
Aya Ben Taarit <taarit.aya@gmail.com>
2022-06-03 16:20:03 +0200

    assignment part 2 - add service and show list of recipes

--- src/main/java/guru/springframework/controllers/IndexController.java
+++ src/main/java/guru/springframework/controllers/IndexController.java
@@ -1,33 +1,13 @@
 package guru.springframework.controllers;
 
-import guru.springframework.model.Category;
-import guru.springframework.model.UnitMeasure;
-import guru.springframework.repositories.CategoryRepository;
-import guru.springframework.repositories.UnitMeasureRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.Optional;
 
 @Controller
 public class IndexController {
 
-    private CategoryRepository categoryRepository;
-    private UnitMeasureRepository unitMeasureRepository;
-
-    public IndexController(CategoryRepository categoryRepository, UnitMeasureRepository unitMeasureRepository) {
-        this.categoryRepository = categoryRepository;
-        this.unitMeasureRepository = unitMeasureRepository;   }
-
     @RequestMapping({"","/","/index"})
     public String getIndexPage(){
-
-        Optional<Category> categoryOptional = categoryRepository.findByDescription("American");
-        Optional<UnitMeasure> unitMeasureOptional = unitMeasureRepository.findByDescription("Cup");
-
-
-        System.out.println("Category Id is : " + categoryOptional.get().getId());
-        System.out.println("UOM ID is " + unitMeasureOptional.get().getId());
 
         return "index";
 
--- src/main/java/guru/springframework/controllers/RecipeController.java
+++ src/main/java/guru/springframework/controllers/RecipeController.java
@@ -2,16 +2,21 @@ package guru.springframework.controllers;
 
 import guru.springframework.services.RecipeService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class RecipeController {
 
-    RecipeService recipeService;
+    private final RecipeService recipeService;
+
+    public RecipeController(RecipeService recipeService) {
+        this.recipeService = recipeService;
+    }
 
     @RequestMapping("/recipes")
-    public String getRecipes(){
-
+    public String getRecipes(Model model){
+        model.addAttribute("recipes",recipeService.getRecipes());
         return "recipes";
     }
 }
